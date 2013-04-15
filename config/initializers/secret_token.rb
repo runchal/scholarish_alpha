a65e4f2513dc27ebe7b29f127704547e5fad5424
Amit Runchal <amitrunchal@gmail.com>
2013-04-15 11:45:34 -0700

    initial app creation and commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScholarishAlpha::Application.config.secret_token = '61c6224c712636f2ae3d060aa4c5e2832ad1088a6b69a03935edcc1fd02f141275d869227d24d0b82066a68df394954840059f2969061af3518007f45ccd70f7'
